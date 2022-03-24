21b2158ae608c764938cea912d7e8420118df47b
ant0n <69129717+antonchu2006@users.noreply.github.com>
2022-03-25 00:01:14 +0100

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -27,25 +27,33 @@ def splitter(email):
         
         for y in range(x+1,len(mail)+1):
             mail4 = mail3[:y] + "." + mail3[y:]
+            mail44 = mail4 + "."
+            mail45 = "." + mail4
             if not re.search(r"\.\.", mail4):
-                mail_list.append(mail4) 
-            
-            for z in range(x+1, len(mail)+1):
-                mail5 = mail4[:y] + "." + mail4[y:]
-                if not re.search(r"\.\.", mail4):
-                    mail_list.append(mail4) 
-                if not re.search(r"\.\.", mail5):
-                    mail_list.append(mail5) 
-            
-
+                mail_list.append(mail4)
+            if not re.search(r"\.\.", mail44):
+                mail_list.append(mail44)
+            if not re.search(r"\.\.", mail45):
+                mail_list.append(mail45)
+                          
     
     mail_list = set(mail_list)
     mail_list = list(mail_list)
     
     mail_list.append("." + '.'.join(chars) + ".")
     
+    if not re.search(r"\.\.",mail+"."):
+        mail_list.append(mail+".")
+    if not re.search(r"\.\.","."+mail):
+        mail_list.append("."+mail)
+    
+    mail_list.append(mail)
+    
     return mail_list
     
 
 if __name__ == "__main__":
     splitter("example@example.com")
+    
+    print("[+] Lists: \""+ str(splitter("example@example.com"))+ "\"")
+    print("[+] Mails:" + str(len(splitter("example@example.com"))))
